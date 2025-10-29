96b1205d92a4daadf34435fd3b7c765d8252d7f2
Muhammad Bilal Khan <mb0696652@gmail.com>
2025-10-29 13:11:00 +0500

    a

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Courses from './components/Courses';
+import Impact from './components/Impact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+      <Hero />
+      <Courses />
+      <Impact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
